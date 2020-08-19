ebef403a27e98614df97ef6d75443bfff3412c03
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-08-19 19:31:03 +0300

    #7 Паттерн декоратор - начало реализации

---
+++ src/decorator/Beverage.java
@@ -0,0 +1,16 @@
+package decorator;
+
+public abstract class Beverage {
+
+    private String description = "Базовый напиток";
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public abstract double cost();
+}
